94cb36e1ae77136f78c42d9af41b052a45e6decc
ruin-and-runes <python.coding.ru@gmail.com>
2026-04-01 14:38:14 +0530

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('stats/', views.stats, name='stats'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+]
