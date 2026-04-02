607b587db533b9e5a656d0cc620060bf09178180
ruin-and-runes <python.coding.ru@gmail.com>
2026-04-02 19:20:36 +0530

    Update 0002_auto_20260402_1903.py

--- books/migrations/0002_auto_20260402_1903.py
+++ books/migrations/0002_auto_20260402_1903.py
@@ -7,9 +7,58 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+
         migrations.AddField(
             model_name='book',
             name='times_read',
             field=models.IntegerField(default=0),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='status',
+            field=models.CharField(max_length=20, default='read'),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='progress',
+            field=models.IntegerField(default=0),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='notes',
+            field=models.TextField(blank=True, null=True),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='cover_url',
+            field=models.URLField(blank=True, null=True),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='cover_image',
+            field=models.ImageField(upload_to='covers/', blank=True, null=True),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='is_comfort',
+            field=models.BooleanField(default=False),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='release_date',
+            field=models.DateField(blank=True, null=True),
+        ),
+
+        migrations.AddField(
+            model_name='book',
+            name='is_released',
+            field=models.BooleanField(default=True),
         ),
     ]
