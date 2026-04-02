1c0566dbe9120156ace9066b46a826ab0d361240
ruin-and-runes <python.coding.ru@gmail.com>
2026-04-02 20:10:55 +0530

    Update 0002_auto_20260402_1903.py

--- books/migrations/0002_auto_20260402_1903.py
+++ books/migrations/0002_auto_20260402_1903.py
@@ -5,12 +5,11 @@ class Migration(migrations.Migration):
     dependencies = [
         ('books', '0001_initial'),
     ]
-   
+
     operations = [
-    migrations.AddField(
-        model_name='book',
-        name='status',
-        field=models.CharField(max_length=20, default='read'),
-    ),
-]
-    
+        migrations.AddField(
+            model_name='book',
+            name='times_read',
+            field=models.IntegerField(default=0),
+        ),
+    ]
