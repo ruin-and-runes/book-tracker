380fb4d4890dd06db1ff4383da9b3662cfccd040
ruin-and-runes <python.coding.ru@gmail.com>
2026-04-04 21:33:47 +0530

    Update apps.py

--- books/apps.py
+++ books/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
 
-
 class BooksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'books'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@example.com',
+                password='admin123'
+            )
