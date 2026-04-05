0d68c8858349ab5465301511e60b63b83210ec57
ruin-and-runes <python.coding.ru@gmail.com>
2026-04-05 15:31:30 +0530

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
-from .views import reset_admin
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('stats/', views.stats, name='stats'),
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
-    path('reset-admin/', reset_admin),
-]
+ ]
