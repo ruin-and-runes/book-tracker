66289cdf5ff4535a95f011fc9fa95b107422fc02
ruin-and-runes <python.coding.ru@gmail.com>
2026-04-05 16:06:05 +0530

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from .views import load_books
+from .views import load_tropes
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('stats/', views.stats, name='stats'),
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
     path('load-books/', load_books),
+    path('load-tropes/', load_tropes),
  ]
