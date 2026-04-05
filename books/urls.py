ae128ec2adbf089157a84de7560b522c7d54f23d
ruin-and-runes <python.coding.ru@gmail.com>
2026-04-05 14:26:43 +0530

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('stats/', views.stats, name='stats'),
     path('book/<int:book_id>/', views.book_detail, name='book_detail'),
-    path('reset-admin/', name='reset_admin'),
+    path('reset-admin/', reset_admin),
 ]
